c739cea00fa37f3c912ea5a34af7ed016a565013
jackduong04 <duonganhtuan04@gmail.com>
2024-10-16 16:35:08 +1300

    Add files via upload

---
+++ job_profile.js
@@ -0,0 +1,58 @@
+const navbar = document.querySelector(".navbar");
+const logo_name_color = document.querySelector(".logo_name a");
+const logo_name_short_color = document.querySelector(".logo_name_short a");
+const links_text_color = document.querySelectorAll(".navbar_links");
+const dropdown_icon_color = document.querySelector(".dropdown_svg");
+
+const dropdown_icon = document.querySelector(".navbar_dropdown");
+const dropdown_menu_mobile = document.querySelector(".navbar_menu");
+
+window.addEventListener("scroll", (e) => {
+    const x = window.scrollY;
+
+    if (x > 299) {
+        navbar.classList.add("active");
+        logo_name_color.classList.add("active");
+        logo_name_short_color.classList.add("active");
+        links_text_color.forEach((e) => e.classList.add("active"));
+        dropdown_icon_color.classList.add("active");
+    }
+    else {
+        if (!dropdown_icon.classList.contains("active")) {
+            navbar.classList.remove("active");
+            logo_name_color.classList.remove("active");
+            logo_name_short_color.classList.remove("active");
+            links_text_color.forEach((e) => e.classList.remove("active"));
+            dropdown_icon_color.classList.remove("active");
+        }
+    }
+});
+
+dropdown_icon.addEventListener('click', (e) => {
+    const icon_status = dropdown_icon.classList.toggle("active");
+    if (icon_status == true) {
+        dropdown_menu_mobile.classList.add("active");
+        logo_name_color.classList.add("active");
+        logo_name_short_color.classList.add("active");
+        dropdown_icon_color.classList.add("active");
+    }
+    else {
+        dropdown_menu_mobile.classList.remove("active");
+        const x = window.scrollY;
+        if (x < 299) {
+            logo_name_color.classList.remove("active");
+            logo_name_short_color.classList.remove("active");
+            dropdown_icon_color.classList.remove("active");
+
+            if (x == 0) {
+                navbar.classList.remove("active");
+            }
+        }
+    }
+});
+
+const bfaft_container = document.querySelector(".bfaft_container");
+const img_before = document.querySelector(".img_bf");
+bfaft_container.addEventListener("mousemove", (e) => {
+    img_before.style.width = `${e.layerX}px`;
+});
